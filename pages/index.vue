0eafe1a1d37bc60ca9664f8f4018b747567db22c
daletom <tom@imgix.com>
2021-06-09 17:13:45 -0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <picture>
+      <source 
+        srcset="https://images.prismic.io/dunelm-prod/07106050-62f0-40f5-abf6-bd2d7a3992e7_WK50+HP_asset06a.jpg?auto=compress,format&amp;rect=0,0,396,256&amp;w=396&amp;h=256" 
+        media="(min-width: 960px)" 
+        data-loaded="true"
+      >
+      <source
+        srcset="https://images.prismic.io/dunelm-prod/07106050-62f0-40f5-abf6-bd2d7a3992e7_WK50+HP_asset06a.jpg?auto=compress,format&amp;rect=16,0,364,256&amp;w=296&amp;h=208"
+        media="(min-width: 768px)"
+        data-loaded="true"
+      >
+      <source
+        srcset="https://images.prismic.io/dunelm-prod/9fcc9b61-d651-484b-9c07-b2f76bec2b9c_WK50+HP_asset06b.jpg?auto=compress,format&amp;rect=0,0,234,209&amp;w=234&amp;h=209"
+        media="(min-width: 480px)"
+        data-loaded="true"
+      >
+      <source
+        srcset="https://images.prismic.io/dunelm-prod/9fcc9b61-d651-484b-9c07-b2f76bec2b9c_WK50+HP_asset06b.jpg?auto=compress,format&amp;rect=0,4,234,199&amp;w=216&amp;h=184"
+        media="(min-width: 320px)"
+        data-loaded="true"
+      >
+      <img
+        src="https://images.prismic.io/dunelm-prod/07106050-62f0-40f5-abf6-bd2d7a3992e7_WK50+HP_asset06a.jpg?auto=compress,format&amp;rect=0,0,396,256&amp;w=396&amp;h=256"
+        alt="Speedy seating in just 7 days"
+        data-testid="img"
+        class="dw-1345uxr-LazyImg eulyh3c0"
+        data-loaded="true"
+      >
+    </picture>
+    <picture>
+      <source 
+        srcset="https://images.prismic.io/dunelm-prod/07106050-62f0-40f5-abf6-bd2d7a3992e7_WK50+HP_asset06a.jpg?ch=width,dpr&amp;auto=compress,format&amp;rect=0,0,396,256&amp;w=396&amp;h=256" 
+        media="(min-width: 960px)" 
+        data-loaded="true"
+      >
+      <source
+        srcset="https://images.prismic.io/dunelm-prod/07106050-62f0-40f5-abf6-bd2d7a3992e7_WK50+HP_asset06a.jpg?ch=width,dpr&amp;auto=compress,format&amp;rect=16,0,364,256&amp;w=296&amp;h=208"
+        media="(min-width: 768px)"
+        data-loaded="true"
+      >
+      <source
+        srcset="https://images.prismic.io/dunelm-prod/9fcc9b61-d651-484b-9c07-b2f76bec2b9c_WK50+HP_asset06b.jpg?ch=width,dpr&amp;auto=compress,format&amp;rect=0,0,234,209&amp;w=234&amp;h=209"
+        media="(min-width: 480px)"
+        data-loaded="true"
+      >
+      <source
+        srcset="https://images.prismic.io/dunelm-prod/9fcc9b61-d651-484b-9c07-b2f76bec2b9c_WK50+HP_asset06b.jpg?ch=width,dpr&amp;auto=compress,format&amp;rect=0,4,234,199&amp;w=216&amp;h=184"
+        media="(min-width: 320px)"
+        data-loaded="true"
+      >
+      <img
+        src="https://images.prismic.io/dunelm-prod/07106050-62f0-40f5-abf6-bd2d7a3992e7_WK50+HP_asset06a.jpg?ch=width,dpr&amp;auto=compress,format&amp;rect=0,0,396,256&amp;w=396&amp;h=256"
+        alt="Speedy seating in just 7 days"
+        data-testid="img"
+        class="dw-1345uxr-LazyImg eulyh3c0"
+        data-loaded="true"
+      >
+    </picture>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
